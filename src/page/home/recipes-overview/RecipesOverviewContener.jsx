3b4fa4525c97ba4251a33c115ea3d53eaee0c233
rofikul-resan <resan6231@gmail.com>
2023-05-02 21:44:15 +0600

    add new section on home page

--- src/page/home/recipes-overview/RecipesOverviewContener.jsx
+++ src/page/home/recipes-overview/RecipesOverviewContener.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import FoodCard from "../../recipes/FoodCard";
+
 const RecipesOverviewContener = () => {
+  const [recipe, setRecipe] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/foods")
+      .then((res) => res.json())
+      .then((data) => setRecipe(data));
+  }, []);
   return (
     <div>
-      <div className="w-3/5 mx-auto text-white text-center">
+      <div className="w-3/5 mx-auto py-10 text-white text-center">
         <h1 className="mb-6">Our Recipes</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
@@ -11,7 +21,20 @@ const RecipesOverviewContener = () => {
           iure molestiae ab?
         </p>
       </div>
-      <div></div>
+      <div className="md:w-4/5 mx-auto border rounded-md p-3 mt-6 border-white">
+        <div className="flex justify-between text-white border-b pb-1 border-white">
+          <p>Our recipe</p>{" "}
+          <Link className="underline hover:text-orange-500" to={"/recipes"}>
+            {" "}
+            see more +
+          </Link>
+        </div>
+        <div className="grid grid-cols-3 gap-4 mt-6">
+          {recipe.slice(7, 10).map((food) => (
+            <FoodCard key={food.idMeal} food={food} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
